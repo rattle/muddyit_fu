e6e971c9f906ee7bf263c027f683721e1139837a
robl <robl@monkeyhelper.com>
2010-10-14 11:06:03 +0100

    Added oauth gem fixes

--- lib/muddyit_fu.rb
+++ lib/muddyit_fu.rb
@@ -16,6 +16,26 @@ module OAuth
   VERSION = "0.4.2"
 end
 
+# Fix for broken oauth gem
+class String
+  
+  # these are to backport methods from 1.8.7/1.9.1 to 1.8.6
+  
+  unless method_defined?(:bytesize)
+    def bytesize
+      self.size
+    end
+  end
+  
+  unless method_defined?(:bytes)
+    def bytes
+      require 'enumerator'
+      Enumerable::Enumerator.new(self, :each_byte)
+    end
+  end
+  
+end
+
 class Module
   def class_attr_accessor(attribute_name)
     class_eval <<-CODE
